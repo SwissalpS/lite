65550651fbcb1461b9093e861d1752ec8af70c12
SwissalpS <Luke@SwissalpS.ws>
2020-06-03 00:46:43 +0200

    oopsie

--- data/user/keymap.lua
+++ data/user/keymap.lua
@@ -2,5 +2,5 @@ local keymap = require "core.keymap"
 
 -- key binding:
 keymap.add { ["ctrl+q"] = "core:quit" }
-keymap.add { ["keypad enter"] = { "command:submit", "doc:newline" } }, true
+keymap.add({ ["keypad enter"] = { "command:submit", "doc:newline" } }, true)
 

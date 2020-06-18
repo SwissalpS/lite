2d60f6f01899bddc7078ae89f951ed342dbecfbd
SwissalpS <Luke@SwissalpS.ws>
2020-06-18 17:34:16 +0200

    detect more php files

--- data/plugins/language_php.lua
+++ data/plugins/language_php.lua
@@ -2,6 +2,7 @@ local syntax = require "core.syntax"
 
 syntax.add {
   files = { "%.php$", "%.phtml", "%.phps$" },
+  headers = "^<%?php",
   comment = "//",
   patterns = {
     { pattern = "//.-\n",                 type = "comment"  },
